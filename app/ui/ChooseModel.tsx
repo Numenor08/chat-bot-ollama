c17cb447952fcfda4d5ffd8674812304c54d0e61
Athaya Razan <142677331+Numenor08@users.noreply.github.com>
2025-03-21 21:46:48 +0700

    styles(topbar): change choose model component sytle

--- app/ui/ChooseModel.tsx
+++ app/ui/ChooseModel.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { TriangleDownIcon } from "@radix-ui/react-icons";
 import { abel } from "@/app/fonts";
 import { useModelContext } from "@/app/store/ContextProvider";
+import { DropdownMenu, DropdownItem } from "@/app/ui/Dropdown";
 
 const ChooseModel = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -12,29 +13,35 @@ const ChooseModel = () => {
         setIsOpen(!isOpen);
     };
 
-    const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        setModel(e.target.value);
+    const handleSelectModel = (name: string) => {
+        setModel(name);
+        setIsOpen(false);
     }
 
     return (
-        <div className={`${abel.className} absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 min-w-44 flex bg-white border overflow-y-auto overflow-x-hidden border-gray-300 rounded-3xl shadow`}>
-            <select
-                className="appearance-none focus:outline-none p-1 pl-4 pr-10"
+        <>
+            <button
                 onClick={handleSelectClick}
-                onBlur={() => setIsOpen(false)}
-                onChange={handleSelectChange}
-                value={model ? model : 'no-model'}
-            >
-                {!listModels ? (
-                    <option value={'no-model'} disabled>No models installed</option>
-                ) : (
-                    listModels.map((model, i) => (
-                        <option value={model.name} key={i}>{model.name}</option>
-                    ))
-                )}
-            </select>
-            <TriangleDownIcon className={`w-5 h-5 absolute right-3 top-1/2 -translate-y-1/2 transition-transform duration-75 ${isOpen ? 'rotate-180' : ''}`} />
-        </div>
+                className={`${abel.className} text-nowrap text-lg text-neutral-700 z-20 cursor-pointer absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-min flex bg-white hover:bg-gray-200 hover:text-neutral-900 overflow-y-auto overflow-x-hidden rounded-lg transition-colors duration-150`}>
+                <div className="p-1 pl-4 pr-10">
+                    {listModels ? model : 'No Model Installed'}
+                </div>
+                <TriangleDownIcon className={`w-5 h-5 absolute right-3 top-1/2 -translate-y-1/2 transition-transform duration-75 ${isOpen ? 'rotate-180' : ''}`} />
+            </button>
+            {isOpen && (
+                <>
+                    <DropdownMenu className="absolute text-neutral-700 top-12 left-1/2 transform -translate-x-1/2 font-mono text-sm w-min max-h-40 flex border overflow-y-auto bg-white rounded-md p-1 z-20 ">
+                        {listModels?.map((model, i) => (
+                            <DropdownItem className="text-nowrap transition-colors duration-100" key={i} onClick={() => handleSelectModel(model.name)}>{model.name}</DropdownItem>
+                        ))}
+                    </DropdownMenu>
+                    <div
+                        className={`fixed inset-0 bg-transparent z-10 ${isOpen ? "opacity-30" : "opacity-0 hidden pointer-events-none"
+                            }`}
+                        onClick={() => setIsOpen(false)}></div>
+                </>
+            )}
+        </>
     );
 };
 
